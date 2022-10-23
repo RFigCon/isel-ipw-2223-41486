c4b093548a409c84f7a1f01a93550ca4e17a6dd0
RFigCon <rafcon96@gmail.com>
2022-10-23 19:14:23 +0100

    update

--- Project_1/Part_2/imdb.mjs
+++ Project_1/Part_2/imdb.mjs
@@ -1,36 +1,39 @@
 //API-Key = k_722ghagn
 import { readFile, writeFile } from 'node:fs/promises'
 import fetch from 'node-fetch'
+import { Console } from 'node:console'
 
 const URL = 'https://imdb-api.com/en/API/Title/k_722ghagn/'
 const IN_FILE = './movie_request.json'
 const OUT_FILE = 'movies.json'
 
-async function main(){
-    let id_list = await get_ids()
-    let movie_list = await make_all_requests(id_list)
+
+//Async/await
+async function main_aw(){
+    let st = Date.now()
+    let id_list = await get_ids_aw()
+    let movie_list = await make_all_requests_aw(id_list)
     write_file(movie_list)
+    let end = Date.now()
+    console.log(end-st)
 }
 
-async function get_ids(){
+async function get_ids_aw(){
     let fileContent = await readFile(IN_FILE)
     
     return  JSON.parse(fileContent)['movie-ids']
 }
 
-async function make_all_requests(id_list){
-    return Promise.all(id_list.map(new_url))
+function make_all_requests_aw(id_list){
+    return Promise.all(id_list.map(make_request_aw))
 }
 
-function new_url(id){
-    return make_request(URL + id)
-}
-
-async function make_request(url) {
+async function make_request_aw(id) {
+    let url = URL + id
     try {
-        let response = await fetch(url)
-        //let response = await internal_id_fetch(URL)
-        return await response.json()
+        //let response = await fetch(url)
+        let response = await internal_id_fetch(url)
+        return await response//.json()
     } catch(err) {
         return err
     }
@@ -53,13 +56,70 @@ function write_file(movie_list) {
     writeFile(OUT_FILE, JSON.stringify(final_list, null, 4))
 }
 
-main()
 
+
+//Promisses
+function main_pr(){
+    get_ids_pr()
+    .then((id_list) => make_all_requests_pr(id_list))
+    .then((movie_list) => write_file(movie_list))
+}
+
+function get_ids_pr(){
+    let ret = readFile(IN_FILE)
+                .then((fileContent) => {
+                    JSON.parse(fileContent)['movie-ids']
+                }).then()
+    return ret
+
+    //return fileContent = await readFile(IN_FILE)
+    //return  JSON.parse(fileContent)['movie-ids']
+}
+
+function make_all_requests_pr(id_list){
+
+    console.log("Test make_all_requests_pr/id_list = " + id_list)
+
+    return new Promise((resolve, reject) => {
+
+        let arr = []
+        for (let index = 0; index < id_list.length; index++) {
+            arr[index] = make_request_pr(id_list[index])
+        }
+
+        resolve(arr)
+    })
+    
+
+    //return Promise.all(id_list.map(make_request_pr))
+}
+
+function make_request_pr(id) {
+    let url = URL + id
+    try {
+        //return fetch(url).then(response => response.json())
+        return internal_id_fetch(url)
+    } catch(err) {
+        return err
+    }
+    
+}
+
+main_aw()
+//main_pr()
+
+//Simulate fetch
 function internal_id_fetch(url){
     let jsonObj = {
         id:	"tt0111161",
         title:	"The Shawshank Redemption",
         runtimeMins:	"142"
     }
-    return jsonObj
+    return new Promise((resolve, reject) =>
+                        {
+                            setTimeout(() => {
+                                resolve(jsonObj)
+                            }, 1000)
+                        })
+    //return jsonObj
 }
